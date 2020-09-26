33dbacb2f9a9ef575a084ae0e4a036e13e18a4bc
Camila Zelenovsky <camila_zele@hotmail.com>
2020-09-26 14:29:45 -0300

    first commit

---
+++ Repository/BaseRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XNews.Repository
+{
+    public class BaseRepository
+    {
+        public string _connectionString = "Data Source=CAMILA\\SQLEXPRESS; Initial Catalog=Erecruta; Integrated Security=True";
+    }
+}
